cd643a250add7d8db277f3ca7abbc62cd2e0aa22
Simbaxo <ballerlikemike23@gmail.com>
2025-11-24 20:11:31 -0500

    edit button

--- blogging website - frontend/src/components/navbar.component.jsx
+++ blogging website - frontend/src/components/navbar.component.jsx
@@ -1,7 +1,11 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import logo from "../imgs/logo.png";
 
 const Navbar = () => {
+
+    const [ searchBoxVisibility, setSearchBoxVisibility ] = useState(false);
+
     return (
         <nav className="navbar">
 
@@ -9,7 +13,7 @@ const Navbar = () => {
                 <img src={logo} alt="Logo" className="w-full" />
             </Link>
 
-            <div className="absolute bg-white w-full left-0 top-full mt-0.5 border-b border-grey py-4 px-[5vw] md:border-0 md:block md:relative md:inset-0 md:p-0 md:w-auto">
+            <div className={"absolute bg-white w-full left-0 top-full mt-0.5 border-b border-grey py-4 px-[5vw] md:border-0 md:block md:relative md:inset-0 md:p-0 md:w-auto md:show" + ( searchBoxVisibility ? "show" : "hide")}>
                 <input
                     type="text"
                     placeholder="Search"
@@ -20,9 +24,16 @@ const Navbar = () => {
             </div>
 
             <div className="flex items-center gap-3 md:gap-6 ml-auto">
-                <button className="md:hidden bg-grey w-12 h-12 rounded-full flex items-center justify-center">
+                <button className="md:hidden bg-grey w-12 h-12 rounded-full flex items-center justify-center"
+                        onClick={ () => setSearchBoxVisibility(currentVal => !currentVal) }
+                >
                     <i className="fi fi-rr-search text-xl"></i>
                 </button>
+
+                <Link to="/editor" className="hidden md:flex gap-2 link">
+                    <i className="fi fi-rr-file-edit"></i>
+                    <p>Write</p>
+                </Link>
             </div>
 
         </nav>    
